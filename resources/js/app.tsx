19912dfbbc49a80f4b8b5f41d595dca7e4b69b32
sulthanalif <sulthanalif45@gmail.com>
2025-04-21 08:44:49 +0700

    <update>[add react vite]

---
+++ resources/js/app.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => <h1>Hello from React + TS!</h1>;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
